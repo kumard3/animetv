0a61e4b259046541ad24991557003f08b0d4cac8
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2021-06-27 16:41:38 +0530

    cardInfo updated

--- src/components/CardInfo.js
+++ src/components/CardInfo.js
@@ -8,44 +8,38 @@ import { Link } from "react-router-dom";
 const CardInfo = ({ allData }) => {
   const { id } = useParams();
   const animeData = allData.filter((book) => book.id === parseInt(id));
-  console.log(allData);
-  return (
+  const anime = animeData[0];
+  console.log(anime);
 
-
-
-    <div className="card__info" >
-      {animeData.map((anime) => {
-        return (
-          <>
-          <Link to='/'>
-          <img className="arrow" src={arrow}  />
-          </Link>
-                <img
-                  className="cardinfo__img"
-                  src={`https://image.tmdb.org/t/p/original${anime.poster_path}`}
-                  alt="card-img"
-                />
-                <div className="cardinfo__wrapper">
-                  <h2>{anime.title} </h2>
-                  <div >
-                    {" "}
-                    <span className="rating " >{anime.popularity}</span>{" "}
-                  </div>
-                  <div className="genes">
-                    <span> Adventure </span>
-                    <span> Drama </span>
-                    <span> Adventure </span>
-                  </div>
-                  <p> 
-                  {anime.overview}
-                  </p>
-                </div>
-
-          </>
-          )
-      })}
-    </div>
-  );
+  if (anime) {
+    return (
+      <div className="card__info">
+        <Link to="/">
+          <img className="arrow" src={arrow} />
+        </Link>
+        <img
+          className="cardinfo__img"
+          src={`https://image.tmdb.org/t/p/original${anime.poster_path}`}
+          alt="card-img"
+        />
+        <div className="cardinfo__wrapper">
+          <h2>{anime.title} </h2>
+          <div>
+            {" "}
+            <span className="rating ">{anime.popularity}</span>{" "}
+          </div>
+          <div className="genes">
+            <span> Adventure </span>
+            <span> Drama </span>
+            <span> Adventure </span>
+          </div>
+          <p>{anime.overview}</p>
+        </div>
+      </div>
+    );
+  } else {
+    return null;
+  }
 };
 
 export default CardInfo;
